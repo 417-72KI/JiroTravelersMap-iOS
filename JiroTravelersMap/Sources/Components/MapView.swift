87abff07caa09b8a6acccedce924aa1c8ac30c5b
417-72KI <417.72ki@gmail.com>
2020-02-03 00:51:40 +0900

    LocationManager wrapper

--- JiroTravelersMap/Sources/Components/MapView.swift
+++ JiroTravelersMap/Sources/Components/MapView.swift
@@ -4,12 +4,22 @@ import MapKit
 struct MapView: UIViewRepresentable {
     typealias UIViewType = MKMapView
 
+    @ObservedObject private var lm = LocationManager()
+    @State var annotations: [MKAnnotation] = []
+
     func makeUIView(context: UIViewRepresentableContext<MapView>) -> MKMapView {
         .init(frame: .zero)
     }
 
-    func updateUIView(_ uiView: MKMapView, context: UIViewRepresentableContext<MapView>) {
-        
+    func updateUIView(_ mapView: MKMapView, context: UIViewRepresentableContext<MapView>) {
+        mapView.showsUserLocation = true
+
+        if lm.isAuthorized,
+            let location = lm.location {
+            let span = MKCoordinateSpan(latitudeDelta: 0.009, longitudeDelta: 0.009)
+            let region = MKCoordinateRegion(center: location.coordinate, span: span)
+            mapView.setRegion(region, animated: true)
+        }
     }
 }
 
---
+++ JiroTravelersMap/Sources/Util/LocationManager.swift
@@ -0,0 +1,56 @@
+import Foundation
+import Combine
+import CoreLocation
+
+final class LocationManager: NSObject, ObservableObject {
+    private let locationManager: CLLocationManager = .init()
+
+    @Published var isAuthorized: Bool
+    @Published var location: Location?
+
+    override init() {
+        isAuthorized = false
+        super.init()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+
+    deinit {
+        locationManager.stopUpdatingLocation()
+    }
+}
+
+extension LocationManager: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        self.isAuthorized = status.isAuthorized
+    }
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        self.location = locations.first.flatMap(Location.init)
+    }
+}
+
+// MARK: -
+extension CLAuthorizationStatus {
+    var isAuthorized: Bool {
+        switch self {
+        case .authorizedAlways, .authorizedWhenInUse:
+            return true
+        default:
+            return false
+        }
+    }
+}
+
+// MARK: -
+extension Location {
+    init(_ location: CLLocation) {
+        self.init(lat: location.coordinate.latitude, lng: location.coordinate.longitude)
+    }
+
+    var coordinate: CLLocationCoordinate2D { .init(latitude: lat, longitude: lng) }
+
+    func asCLLocation() -> CLLocation { .init(latitude: lat, longitude: lng) }
+}
