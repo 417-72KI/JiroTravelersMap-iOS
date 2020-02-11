d97a7cabea44d75b55211f45148ac753d4f1ef88
417-72KI <417.72ki@gmail.com>
2020-02-11 22:16:01 +0900

    Layout

--- JiroTravelersMap/Sources/View/Detail/ShopDetailView.swift
+++ JiroTravelersMap/Sources/View/Detail/ShopDetailView.swift
@@ -4,23 +4,24 @@ struct ShopDetailView: View {
     let shop: Shop
 
     var body: some View {
-        NavigationView {
-            VStack {
-                Text("住所")
-                Text(shop.address)
-                Divider()
-                Text("定休日")
-                Text(shop.regularHoliday.shortStringValue)
-                Divider()
-                Text("営業時間")
-                Text(shop.openingHours.stringValue)
-                Divider()
-            }
-        }.navigationBarTitle(shop.name)
+        VStack(alignment: .leading, spacing: 8) {
+            Text("住所")
+            Text(shop.address)
+            Divider()
+            Text("定休日")
+            Text(shop.regularHoliday.shortStringValue)
+            Divider()
+            Text("営業時間")
+            Text(shop.openingHours.stringValue)
+            Spacer()
+        }.padding(20)
+            .navigationBarTitle(shop.name)
             .navigationBarItems(trailing:
                 Button(action: {
-                }) { Image(R.image.ic_create)
-                }
+                    // TODO: New memory view
+                },
+                       label: { Image(R.image.ic_create) }
+                )
         )
     }
 }
