35eb82c610410112e93020eedea8050e1b532652
417-72KI <417.72ki@gmail.com>
2020-02-15 17:03:01 +0900

    map from detail

---
+++ JiroTravelersMap/Sources/Components/BackButton.swift
@@ -0,0 +1,24 @@
+import SwiftUI
+
+struct BackButton: View {
+    var label: String
+    let action: () -> Void
+
+    var body: some View {
+        Button(action: { self.action() }) {
+            HStack {
+                Image(systemName: "chevron.left")
+                Text(label)
+            }
+        }
+    }
+}
+
+#if DEBUG
+struct BackButton_Previews: PreviewProvider {
+    static var previews: some View {
+        BackButton(label: "Back") {
+        }
+    }
+}
+#endif
--- JiroTravelersMap/Sources/View/Detail/ShopDetailView.swift
+++ JiroTravelersMap/Sources/View/Detail/ShopDetailView.swift
@@ -3,19 +3,27 @@ import SwiftUI
 struct ShopDetailView: View {
     let shop: Shop
 
+    @State private var showMap: Bool = false
+
     var body: some View {
         VStack(alignment: .leading, spacing: 8) {
+            NavigationLink(
+                destination: ShopMapView(shopList: [shop])
+                    .navigationBarBackButtonHidden(true)
+                    .navigationBarItems(leading:
+                        BackButton(label: shop.name) { self.showMap = false }
+                ),
+                isActive: $showMap,
+                label: { EmptyView() }
+            )
             HStack {
                 VStack(alignment: .leading, spacing: 8) {
                     Text("住所")
                     Text(shop.address)
                 }
                 Spacer()
-                Button(action: {
-                    // TODO: Map View
-                },
-                       label: { Image(R.image.ic_map) }
-                )
+                Button(action: { self.showMap = true },
+                       label: { Image(R.image.ic_map) })
             }
             Divider()
             Text("定休日")
