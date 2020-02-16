e988cc9ca9c2bfd8c9382f7c53a33e2768461a05
417-72KI <417.72ki@gmail.com>
2020-02-17 07:45:57 +0900

    bug fixed in real devices

--- JiroTravelersMap/Sources/View/Detail/ShopDetailView.swift
+++ JiroTravelersMap/Sources/View/Detail/ShopDetailView.swift
@@ -3,28 +3,17 @@ import SwiftUI
 struct ShopDetailView: View {
     let shop: Shop
 
-    @State private var showMap: Bool = false
 
     var body: some View {
-        // swiftlint:disable:next closure_body_length
         VStack(alignment: .leading, spacing: 8) {
-            NavigationLink(
-                destination: ShopMapView(shopList: [shop])
-                    .navigationBarBackButtonHidden(true)
-                    .navigationBarItems(leading:
-                        BackButton(label: shop.name) { self.showMap = false }
-                ),
-                isActive: $showMap,
-                label: { EmptyView() }
-            )
             HStack {
                 VStack(alignment: .leading, spacing: 8) {
                     Text("住所")
                     Text(shop.address)
                 }
                 Spacer()
-                Button(action: { self.showMap = true },
-                       label: { Image(R.image.ic_map) })
+                NavigationLink(destination: ShopMapView(shopList: [shop]),
+                               label: { Image(R.image.ic_map) })
             }
             Divider()
             Text("定休日")
