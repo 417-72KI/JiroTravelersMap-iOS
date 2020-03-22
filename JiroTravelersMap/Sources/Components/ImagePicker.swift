5a631bc8d74673bbfc3496e9e6afc6865920dee4
417-72KI <417.72ki@gmail.com>
2020-03-23 01:32:46 +0900

    [wip] image picker

---
+++ JiroTravelersMap/Sources/Components/ImagePicker.swift
@@ -0,0 +1,43 @@
+import SwiftUI
+import UIKit
+
+struct ImagePicker: UIViewControllerRepresentable {
+    @Binding var image: UIImage?
+
+    init(image: Binding<UIImage?>) {
+        _image = image
+    }
+
+    func makeCoordinator() -> Coordinator { .init(image: _image) }
+
+    func makeUIViewController(context: UIViewControllerRepresentableContext<ImagePicker>) -> UIImagePickerController {
+        let imagePickerViewController = UIImagePickerController()
+        imagePickerViewController.sourceType = .photoLibrary
+        imagePickerViewController.delegate = context.coordinator
+        return imagePickerViewController
+    }
+
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: UIViewControllerRepresentableContext<ImagePicker>) {
+        // no-op
+    }
+}
+
+extension ImagePicker {
+    final class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+        @Binding var image: UIImage?
+
+        init(image: Binding<UIImage?>) {
+            _image = image
+        }
+
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+            defer { picker.dismiss(animated: true) }
+
+            image = info[.originalImage] as? UIImage
+        }
+
+        func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+            picker.dismiss(animated: true)
+        }
+    }
+}
